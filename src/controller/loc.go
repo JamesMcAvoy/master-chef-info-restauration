117a83cc6f0eb8476f5588c9b99b222ab9e89cfd
Victor Petitjean <i@fuckmy.cat>
2018-12-13 14:41:26 +0100

    Exécution de toutes les entités à la vitesse désirée

--- src/controller/loc.go
+++ src/controller/loc.go
@@ -8,15 +8,16 @@ import (
 
 // Struct restaurant
 type Resto struct {
-	Win      *view.Window
-	Temps    int
-	accel    int
-	tick     <-chan time.Time
-	Pause    bool
-	Horaires [][2]float64
-	Entrees  []string
-	Plats    []string
-	Desserts []string
+	Win       *view.Window
+	Temps     int
+	accel     int
+	tick      <-chan time.Time
+	Pause     bool
+	Horaires  [][2]float64
+	Entrees   []string
+	Plats     []string
+	Desserts  []string
+	Personnes []Personne
 }
 
 // Constructeur de restaurant
@@ -34,21 +35,19 @@ func NewResto(width, height, temps, accel, i int, pause bool, h [][2]float64, e,
 		Desserts: d,
 	}
 	resto.tick = time.Tick(time.Second / time.Duration(accel))
-	go resto.incTick()
-	go tmpFuncPourTesterLaffichage(&resto)
+	go resto.loop()
+	resto.Personnes = append(resto.Personnes, NewClient(&resto))
 	return &resto
 }
 
-func tmpFuncPourTesterLaffichage(r *Resto) {
-	sprite := r.Win.NewSprite("ressources/serveur.png")
-	sprite.Matrix = sprite.Matrix.Scaled(r.Win.Window.Bounds().Center(), 0.5)
-}
-
 // Incrémente le temps dans le restaurant
-func (r *Resto) incTick() {
+func (r *Resto) loop() {
 	for {
 		select {
 		case <-r.tick:
+			for _, p := range r.Personnes {
+				p.Act()
+			}
 			if r.Pause == false {
 				if r.Temps < 86400 {
 					r.Temps++
--- src/controller/personne.go
+++ src/controller/personne.go
@@ -1,5 +1,25 @@
 package controller
 
-type personne interface {
-	act()
+import (
+	"fmt"
+	"github.com/JamesMcAvoy/resto/src/view"
+)
+
+type Personne interface {
+	Act()
+}
+
+type Client struct {
+	Sprite *view.Sprite
+}
+
+func NewClient(r *Resto) Client {
+	var c Client
+	c.Sprite = r.Win.NewSprite("ressources/LeStig.png")
+	c.Sprite.Matrix = c.Sprite.Matrix.Scaled(r.Win.Window.Bounds().Center(), 0.1)
+	return c
+}
+
+func (c Client) Act() {
+	fmt.Println("bonjour je suis un client")
 }
